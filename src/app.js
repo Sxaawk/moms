98a69bafc588a01c63bbdc613e99380d7b710043
Sxaawk <60027224+Sxaawk@users.noreply.github.com>
2026-05-02 20:05:28 -0600

    Rename TitanBot to moms in app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ class TitanBot extends Client {
 
   async start() {
     try {
-      startupLog('Starting TitanBot...');
+      startupLog('Starting moms...');
       await new Promise(resolve => setTimeout(resolve, 1000));
       
       startupLog('Initializing database...');
@@ -184,7 +184,7 @@ class TitanBot extends Client {
 
     app.get('/', (req, res) => {
       res.status(200).json({ 
-        message: 'TitanBot System Online',
+        message: 'moms System Online',
         version: '2.0.0',
         timestamp: new Date().toISOString()
       });
@@ -356,7 +356,7 @@ class TitanBot extends Client {
 }
 
 try {
-  const bot = new TitanBot();
+  const bot = new moms();
   
   const setupShutdown = () => {
     process.on('SIGTERM', () => bot.shutdown('SIGTERM'));
@@ -380,7 +380,7 @@ try {
   process.exit(1);
 }
 
-export default TitanBot;
+export default moms;
 
 
 
